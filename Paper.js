45840c42b39963d1317ab65faf26e76589277a8d
RitupaSaha <ritup.saha@gmail.com>
2020-09-14 12:47:19 +0530

    changes all the files

--- Paper.js
+++ Paper.js
@@ -1,29 +1,35 @@
-class Paper{
-    consructor(x,y,radius){
-        var options = {
-            isStatic:false,
-            restitution:0.3,
-            friction:0.5,
-            density:1.2
-        }
-        this.radius = radius;
-        this.x = x;
-        this.y = y;
-        this.body = Bodies.circle(x,y,radius,options)
-        World.add(world,this.body)
-    }
-    display() {
-        console.log(this.body);
-        var pos = this.body.position;
-        push();
-        translate(pos.x,pos.y);
-        rectMode(CENTER);
-        strokeWeight(3);
-        fill(255,0,255);
-        ellipse(0,0,this.radius,this.radius);
-        pop();
-    }
-      
+class paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0.5,
+			density:1.2
+
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var paperpos=this.body.position;		
+
+			push()
+			translate(paperpos.x, paperpos.y);
+			rectMode(CENTER)
+			strokeWeight(3);
+			fill(255,0,255)
+			ellipse(0,0,this.r, this.r);
+			pop()
+			
+	}
+
 }
-
-
--- sketch.js
+++ sketch.js
@@ -3,49 +3,63 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj, paperObject,groundObject	
+var world;
 
-function preload()
-{
-	
-}
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
 
 	engine = Engine.create();
 	world = engine.world;
+	dustbinObj=new dustbin(1200,650);
+	paperObject=new paper(200,450,40);
+	groundObject=new ground(width/2,670,width,20);
+	//Create a Ground
+	
 
-	ground = new Dustbin(400,690,800,20);
-	ground.shapeColor = color(243,191,202);
-	
-	paper = new Paper(100,200,10);
-	
-	dustbin1 = new Dustbin(600,670,200,20);
-		
-	dustbin2 = new Dustbin(500,630,20,100);
-		
-	dustbin3 = new Dustbin(700,630,20,100);
-	
+	var render = Render.create({
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1200,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
+
 	Engine.run(engine);
+	//Render.run(render);
+  
 }
+
 
 function draw() {
   rectMode(CENTER);
   background(0);
-  Engine.update(engine);
-  paper.display();
-  ground.display();
-  dustbin1.display();
-  dustbin2.display();
-  dustbin3.display();
-
+ 
+  dustbinObj.display();
+  paperObject.display();
+  groundObject.display();
+  
+ 
+  
+  
+ 
 }
 
 function keyPressed() {
-	if(keyCode === UP_ARROW) {
-		Matter.Body.applyForce(paper.body,paper.body.position,{x:85,y:-85});
-	}
+  	if (keyCode === UP_ARROW) {
+
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+    
+  	}
 }
+
+
 
 
 
